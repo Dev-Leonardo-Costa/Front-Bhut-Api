d5f6be768a8f21ded07c51bea55df3cb0978622f
Leonardo Costa <devleonardo27@gmail.com>
2023-02-08 12:39:51 -0300

    home implementado com rota

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,16 @@ import { HeaderComponent } from "./components/template/header/header.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { FooterComponent } from "./components/template/footer/footer.component";
 import { NavComponent } from "./components/template/nav/nav.component";
+import { HomeComponent } from './components/views/home/home.component';
 
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatSidenavModule } from "@angular/material/sidenav";
 import { MatListModule } from "@angular/material/list";
 import { MatIconModule } from "@angular/material/icon";
+import {MatCardModule} from '@angular/material/card';
+
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, FooterComponent, NavComponent],
+  declarations: [AppComponent, HeaderComponent, FooterComponent, NavComponent, HomeComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -22,6 +25,7 @@ import { MatIconModule } from "@angular/material/icon";
     MatSidenavModule,
     MatListModule,
     MatIconModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent],
